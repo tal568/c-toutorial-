e3ea03f4eb057f2f2371a140af189f3f2b21fa6b
John Doe <johndoe@example.com>
2022-04-16 22:52:59 +0300

    printing in cpp

---
+++ FirstCppProgram/FirstCpp.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello word!" << std::endl;
+    std::cout <<"first frogram" << std::endl <<" ok this is the new line" <<std::endl;
+
+    return 0;
+}
