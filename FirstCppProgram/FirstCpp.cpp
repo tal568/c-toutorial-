2925348c869b5bf4d931d0b12009d301eeb6edae
John Doe <johndoe@example.com>
2022-04-16 23:00:41 +0300

    prins and comants

--- FirstCppProgram/FirstCpp.cpp
+++ FirstCppProgram/FirstCpp.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-
+// the laibary that do the input out put
 int main()
 {
     std::cout << "Hello word!" << std::endl;
-    std::cout <<"first frogram" << std::endl <<" ok this is the new line" <<std::endl;
+    std::cout << "first frogram" << std::endl
+              << " ok this is the new line" << std::endl;
 
     return 0;
 }
